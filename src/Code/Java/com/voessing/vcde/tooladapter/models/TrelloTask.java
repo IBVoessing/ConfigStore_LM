2c0b1262a14cad68da92ba8c428ecacf91f80f15
Leonardo <leonardo.malzacher@voessing.de>
2024-04-17 11:35:24 +0200

    "minified" the TrelloTask Model to make it more clear to understand as we currently dont need custom getter and setters

--- src/Code/Java/com/voessing/vcde/tooladapter/models/TrelloTask.java
+++ src/Code/Java/com/voessing/vcde/tooladapter/models/TrelloTask.java
@@ -1,98 +1,28 @@
 package com.voessing.vcde.tooladapter.models;
-
 import java.util.List;
 
 public class TrelloTask {
 
-	public static class Card {
-		private String name;
-		private String desc;
-		private String due;
-		private String start;
-		private List<String> idMembers;
+	public Card card;
+
+	static class Card {
+		public String name;
+		public String desc;
+		public String due;
+		public String start;
+		public List<String> idMembers;
 		
-		public String getName() {
-			return name;
-		}
-		public void setName(String name) {
-			this.name = name;
-		}
-		public String getDesc() {
-			return desc;
-		}
-		public void setDesc(String desc) {
-			this.desc = desc;
-		}
-		public String getDue() {
-			return due;
-		}
-		public void setDue(String due) {
-			this.due = due;
-		}
-		public String getStart() {
-			return start;
-		}
-		public void setStart(String start) {
-			this.start = start;
-		}
-		public List<String> getIdMembers() {
-			return idMembers;
-		}
-		public void setIdMembers(List<String> idMembers) {
-			this.idMembers = idMembers;
-		}
-		public List<Checklist> getChecklists() {
-			return checklists;
-		}
-		public void setChecklists(List<Checklist> checklists) {
-			this.checklists = checklists;
-		}
-		private List<Checklist> checklists;
+		public List<Checklist> checklists;
 		
-		public static class Checklist {
-			private String name;
-			private List<CheckItem> checkItems;
-			
-			public String getName() {
-				return name;
-			}
-			public void setName(String name) {
-				this.name = name;
-			}
-			public List<CheckItem> getCheckItems() {
-				return checkItems;
-			}
-			public void setCheckItems(List<CheckItem> checkItems) {
-				this.checkItems = checkItems;
-			}
-
+		static class Checklist {
+			public String name;
+			public List<CheckItem> checkItems;
 		}
 
-		public static class CheckItem {
-			private String name;
-			private String due;
-			private String idMember;
-			
-			public String getName() {
-				return name;
-			}
-			public void setName(String name) {
-				this.name = name;
-			}
-			public String getDue() {
-				return due;
-			}
-			public void setDue(String due) {
-				this.due = due;
-			}
-			public String getIdMember() {
-				return idMember;
-			}
-			public void setIdMember(String idMember) {
-				this.idMember = idMember;
-			}
-
+		static class CheckItem {
+			public String name;
+			public String due;
+			public String idMember;
 		}
 	}
-
 }
