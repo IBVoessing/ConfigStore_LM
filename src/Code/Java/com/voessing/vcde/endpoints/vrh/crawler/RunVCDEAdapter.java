4db3fd5682f97b4cb5dd4d6d51bbbbc9491f98aa
Leonardo <leonardo.malzacher@voessing.de>
2024-04-18 12:55:45 +0200

    added the default retrival of the tool and request document
    moved logic to the actual adapter
    
    adapters are now being proberly addressed

--- src/Code/Java/com/voessing/vcde/endpoints/vrh/crawler/RunVCDEAdapter.java
+++ src/Code/Java/com/voessing/vcde/endpoints/vrh/crawler/RunVCDEAdapter.java
@@ -1,65 +1,89 @@
 package com.voessing.vcde.endpoints.vrh.crawler;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.openntf.domino.utils.Factory;
+import org.openntf.domino.utils.Factory.SessionType;
+
+import com.google.gson.Gson;
+import com.ibm.commons.util.io.json.JsonJavaFactory;
 import com.ibm.commons.util.io.json.JsonJavaObject;
+import com.ibm.commons.util.io.json.JsonParser;
+import com.voessing.common.TGlobalConfig;
 import com.voessing.vcde.endpoints.vrh.resources.VCDEShared;
 import com.voessing.vcde.tooladapter.handlers.TrelloHandler;
+import com.voessing.vcde.tooladapter.models.TrelloTask;
+import com.voessing.vcde.tooladapter.models.TrelloTask.Card;
 import com.voessing.xapps.utils.vrh.configs.VrhResourceHandlerConfig;
+import com.voessing.xapps.utils.vrh.exceptions.VrhException;
 import com.voessing.xapps.utils.vrh.handler.VrhHttpHandler;
+
+import lotus.domino.Database;
+import lotus.domino.Document;
+import lotus.domino.NotesException;
+import lotus.domino.Session;
 
 public class RunVCDEAdapter extends VrhHttpHandler {
 
+	Gson gson = new Gson();
+
+	Database db; 
+
+	Document requestDoc, toolDoc;
+
 	@Override
 	protected VrhResourceHandlerConfig provideConfig(VrhResourceHandlerConfig initialConfig, Map<String, String[]> parameterMap) throws Exception {
-		config.setAllowedMethods("POST");
+		config.setAllowedMethods("POST, GET");
 		config.getAllowedOriginsForAccessControl().addAll(VCDEShared.allowedOriginsForAccessControl);
 		return initialConfig;
 	}
 
 	@Override
 	protected void onRequest(HttpServletRequest request) throws Exception {
-		System.out.println("hallooo :)))");
+		Session session = Factory.getSession(SessionType.NATIVE);
+		//db = session.getDatabase(session.getServerName(), TGlobalConfig.getSystemVar(TGlobalConfig.VCDE_CFG_DB));
+		db = session.getDatabase("CN=IBVDNO03/O=IBV/C=DE", TGlobalConfig.getSystemVar(TGlobalConfig.VCDE_CFG_DB));
+	}
+
+	@Override
+	protected String doGet(HttpServletRequest request) throws Exception {
+		TrelloTask trelloTask = new TrelloTask();
+		Card card = new Card();
+		card.name = "Test Card";
+		card.desc = "Test Description";
+		trelloTask.card = card;
+
+		Gson gson = new Gson();
+
+		return gson.toJson(trelloTask);
 	}
 
 	@Override
 	protected String doPost(HttpServletRequest request) throws Exception {
 		//trigger capturedPayloadRaw to be available
 		getRequestPayload(request);
-		
-		String body = capturedPayloadRaw;
-		
-		TrelloHandler trelloHandler = new TrelloHandler();
+		JsonJavaObject body = (JsonJavaObject) JsonParser.fromJson(JsonJavaFactory.instanceEx, capturedPayloadRaw);
+		loadDocuments(body);
 
-		JsonJavaObject card = new JsonJavaObject();
-		card.put("name", "Test Card");
-		card.put("desc", body);
-		card.put("start", dateToIsoString(new Date()));
-		card.put("due", dateToIsoString(addWeekToDate(new Date())));
+		//String adapter = toolDoc.getItemValueString("deploymentType");
+		String adapter = "ADMIN_TASK"; // dev only
+		System.out.println("DEBUG => Adapter: " + adapter);
 		
-		JsonJavaObject task = new JsonJavaObject();
-		task.put("card", card);
-
-		trelloHandler.createAdminTask(task, "0063", false);
-		
-		return new JsonJavaObject().toString();
+		switch (adapter) {
+			case "ADMIN_TASK":
+				return new TrelloHandler().excecute(requestDoc, toolDoc, body).toString();		
+			default:
+				throw new VrhException(404,"Adapter not set in Tool Document!");
+		}
 	}
 
-	public String dateToIsoString(Date date) {
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
-		return sdf.format(date);
-	}
+	private void loadDocuments(JsonJavaObject body) throws NotesException{
+		String requestUNID = body.getString("id");
+		String toolUNID = body.getString("ToolUNID");
 
-	public Date addWeekToDate(Date date) {
-		Calendar c = Calendar.getInstance();
-		c.setTime(date);
-		c.add(Calendar.WEEK_OF_YEAR, 1);
-		return c.getTime();
+		requestDoc = db.getDocumentByUNID(requestUNID);
+		toolDoc = db.getDocumentByUNID(toolUNID);
 	}
-
 }
