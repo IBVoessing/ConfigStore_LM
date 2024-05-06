c307278f9687f38cf1880829625a40ea2f72dda7
Leonardo <leonardo.malzacher@voessing.de>
2024-05-06 10:48:11 +0200

    idc cleaned up database etc

--- src/Code/Java/com/voessing/vcde/endpoints/srs/SrsConfigManagerBean.java
+++
@@ -1,6 +0,0 @@
-package com.voessing.vcde.endpoints.srs;
-
-import com.voessing.utils.srs.service.SrsServiceBean;
-
-public class SrsConfigManagerBean extends SrsServiceBean {
-}
--- src/Code/Java/com/voessing/vcde/endpoints/vrh/crawler/Test.java
+++ src/Code/Java/com/voessing/vcde/endpoints/vrh/crawler/Test.java
@@ -4,9 +4,21 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
-import com.google.gson.JsonObject;
+import org.apache.commons.io.IOUtils;
+import org.apache.hc.client5.http.classic.methods.HttpPost;
+import org.apache.hc.client5.http.entity.mime.MultipartEntityBuilder;
+import org.apache.hc.client5.http.fluent.Request;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.ContentType;
+import org.apache.hc.core5.http.HttpEntity;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
+import org.openntf.domino.utils.Factory;
+import org.openntf.domino.utils.Factory.SessionType;
+
+import com.voessing.api.adapter.TrelloAPI;
+import com.voessing.common.TVAppCredStore2;
 import com.voessing.xapps.utils.vrh.configs.VrhResourceHandlerConfig;
-import com.voessing.xapps.utils.vrh.exceptions.VrhException;
 import com.voessing.xapps.utils.vrh.handler.VrhHttpHandler;
 
 public class Test extends VrhHttpHandler {
@@ -18,27 +30,38 @@ public class Test extends VrhHttpHandler {
 		return initialConfig;
 	}
 
+    private static final String CREDSTORE_KEY = "trello_lm";
+    
 	@Override
 	protected String doGet(HttpServletRequest request) throws Exception {
-		
-		if(request.getMethod().equalsIgnoreCase("HEAD")){
-			return "yeeeey";
-		}
-		
-		System.out.println("Hallo Test");
-		
-		String blah = "{'msg':'hallu'}";
-		
-		if(!blah.isEmpty()){
-			throw new VrhException(400, "hallo");
-		}
-		
-		JsonObject jo = new JsonObject();
-		jo.addProperty("test", "test");
 
-		return jo.toString();
+        TVAppCredStore2 credStore = new TVAppCredStore2(Factory.getSession(SessionType.NATIVE));
+        String apiKey = credStore.getValueByName(CREDSTORE_KEY, "apiKey");
+        String apiToken = credStore.getValueByName(CREDSTORE_KEY, "apiToken");
+        String authHeader = "OAuth oauth_consumer_key=\"" + apiKey + "\", oauth_token=\"" + apiToken + "\"";
+        
+        String boardGumbaId = "662ba0a35610c87d77d9a89c";
+
+		StringBuilder csvBuilder = new StringBuilder();
+		csvBuilder.append("id,name\n");
+		csvBuilder.append("1,John\n");
+		String csv = csvBuilder.toString();
+
+		TrelloAPI api = new TrelloAPI();
+		api.createAttachmentOnCard(boardGumbaId, "test.csv", csv.getBytes());
+		return "ksdfjlsf";
 	}
-	
+
+	// @Override
+	// protected String doGet(HttpServletRequest request) throws Exception {
+	// 	BasicCredentialsProvider provider = new BasicCredentialsProvider();
+	// 	provider.setCredentials(AuthScope.ANY, (Credentials) new UsernamePasswordCredentials("allo", "gumba".toCharArray() ));
+	// 	String gumba = Request.get("http://httpbin.org/get").execute().returnContent().asString(StandardCharsets.UTF_8);
+	// 	System.out.println(gumba);
+
+	// 	return "";
+	// }
+
 	@Override
 	protected String doPost(HttpServletRequest request) throws Exception {
 		
