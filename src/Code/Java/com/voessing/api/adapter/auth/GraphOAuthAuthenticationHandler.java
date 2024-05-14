eb828f9ac252103a6118169c054a5cc3c4778907
Leonardo <leonardo.malzacher@voessing.de>
2024-05-14 10:18:06 +0200

    resstructured and fixed minor things -removed unnecessary header -now throwing a RuntimeException instead of a console stacktrace

--- src/Code/Java/com/voessing/api/adapter/auth/GraphOAuthAuthenticationHandler.java
+++ src/Code/Java/com/voessing/api/adapter/auth/GraphOAuthAuthenticationHandler.java
@@ -6,7 +6,6 @@ import java.util.Arrays;
 import org.apache.hc.client5.http.classic.methods.HttpUriRequestBase;
 import org.apache.hc.client5.http.entity.UrlEncodedFormEntity;
 import org.apache.hc.core5.http.HttpEntity;
-import org.apache.hc.core5.http.message.BasicHeader;
 import org.apache.hc.core5.http.message.BasicNameValuePair;
 
 import com.google.gson.JsonObject;
@@ -19,7 +18,7 @@ public class GraphOAuthAuthenticationHandler extends AuthenticationHandler {
 
     private String token;
     private LocalDateTime tokenExpires = null;
-	
+
     private String clientId;
     private String clientSecret;
     private String tokenUrl;
@@ -27,46 +26,20 @@ public class GraphOAuthAuthenticationHandler extends AuthenticationHandler {
 
     public GraphOAuthAuthenticationHandler(String clientId, String clientSecret, String tokenUrl, String scope) {
         super();
+
         this.client = new HttpClient();
         this.client.setLogLevel(1);
+
         this.clientId = clientId;
         this.clientSecret = clientSecret;
         this.tokenUrl = tokenUrl;
         this.scope = scope;
     }
 
-    private void reauthenticate() {
-        try {
-            HttpEntity ent = new UrlEncodedFormEntity(Arrays.asList(
-                    new BasicNameValuePair("grant_type", "client_credentials"),
-                    new BasicNameValuePair("client_id", clientId),
-                    new BasicNameValuePair("client_secret", clientSecret),
-                    new BasicNameValuePair("scope", scope)));
-
-            Response res = client.post(tokenUrl, ent,
-                    Arrays.asList(new BasicHeader("Content-Type", "application/x-www-form-urlencoded")));
-
-            JsonObject data = res.parseWithGSON();
-            token = data.get("access_token").getAsString();
-
-            if (data.has("expires_in")) {
-                tokenExpires = LocalDateTime.now().plusSeconds(data.get("expires_in").getAsLong());
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private boolean isExpired() {
-        // 60 seconds of leeway
-        return tokenExpires == null || tokenExpires.isBefore(LocalDateTime.now().minusSeconds(60));
-    }
-
     @Override
     public HttpUriRequestBase authenticate(HttpUriRequestBase request) {
         try {
-            reauthenticate();
+            doAuth();
             request.setHeader("Authorization", "Bearer " + token);
             return request;
         } catch (Exception e) {
@@ -80,7 +53,7 @@ public class GraphOAuthAuthenticationHandler extends AuthenticationHandler {
         // Trigger initial authentication lazily and
         // check for expiration to prevent unnecessary requests
         if (token == null || isExpired()) {
-            reauthenticate();
+            doAuth();
         }
 
         request.setHeader("Authorization", "Bearer " + token);
@@ -89,5 +62,33 @@ public class GraphOAuthAuthenticationHandler extends AuthenticationHandler {
     @Override
     public boolean isAuthenticated(Response response) {
         return response.getStatus() != 401 && response.getStatus() != 403;
+    }
+
+    private void doAuth() {
+        try {
+            HttpEntity body = new UrlEncodedFormEntity(
+                    Arrays.asList(
+                            new BasicNameValuePair("grant_type", "client_credentials"),
+                            new BasicNameValuePair("client_id", clientId),
+                            new BasicNameValuePair("client_secret", clientSecret),
+                            new BasicNameValuePair("scope", scope)));
+
+            Response res = client.post(tokenUrl, body);
+
+            JsonObject data = res.parseWithGSON();
+            token = data.get("access_token").getAsString();
+
+            if (data.has("expires_in")) {
+                tokenExpires = LocalDateTime.now().plusSeconds(data.get("expires_in").getAsLong());
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private boolean isExpired() {
+        // 60 seconds of leeway
+        return tokenExpires == null || tokenExpires.isBefore(LocalDateTime.now().minusSeconds(60));
     }
 }
