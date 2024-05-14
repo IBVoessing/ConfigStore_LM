ad1c8d60b358e0933af93c510564bf2458c47ec9
Leonardo <leonardo.malzacher@voessing.de>
2024-05-14 09:59:44 +0200

    adapted TeamsTeamHandler to the new GraphAPI and Gson instead of HCL Json + formatting

--- src/Code/Java/com/voessing/vcde/tooladapter/handlers/TeamsTeamHandler.java
+++ src/Code/Java/com/voessing/vcde/tooladapter/handlers/TeamsTeamHandler.java
@@ -1,129 +1,139 @@
 package com.voessing.vcde.tooladapter.handlers;
 
-import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.ibm.commons.util.io.json.JsonJavaObject;
-import com.voessing.api.adapter.GraphAPI;
+import com.voessing.api.adapter.GraphAPINew;
+import com.voessing.common.TNotesUtil;
+import com.voessing.common.http.HttpUtil;
+import com.voessing.common.http.Response;
 import com.voessing.xapps.utils.vrh.exceptions.VrhException;
 
 import lotus.domino.Document;
 
-public final class TeamsTeamHandler extends BaseHandler{
+public final class TeamsTeamHandler extends BaseHandler {
 
-    private GraphAPI api = new GraphAPI();
+    private GraphAPINew api = new GraphAPINew();
     private Gson gson = new Gson();
 
     public TeamsTeamHandler(ReqBundle reqBundle) {
         super(reqBundle);
     }
-    
-    @Override
-	public JsonJavaObject excecute() throws Exception {
-    	
-//    	JsonObject apiAttributesObj = reqBundle.body.get("apiAttributes").getAsJsonObject();
-//    	Type type = new TypeToken<Map<String, Object>>(){}.getType();
-//    	Map<String, Object> map = gson.fromJson(apiAttributesObj, type);
-    	
-        String teamId = createToolInstance(new HashMap<>());
+
+    @SuppressWarnings("unchecked")
+	@Override
+    public JsonJavaObject excecute() throws Exception {
+
+        JsonObject apiAttributesObj = reqBundle.body.get("apiAttributes").getAsJsonObject();
+
+        Map<String, Object> map = gson.fromJson(apiAttributesObj, Map.class);
+
+        String teamId = createToolInstance(map);
 
         // sleep for 2 seconds to give the API some time to create the team
         Thread.sleep(2000);
-        
-        List<JsonJavaObject> members = getConversationMembers((List<JsonJavaObject>) reqBundle.body.get("apiMembers"));
-        for (JsonJavaObject member : members) {
-            createUser(teamId, member);
+
+        JsonArray members = getConversationMembers(reqBundle.body.get("apiMembers").getAsJsonArray());
+        for (JsonElement member : members) {
+            createUser(teamId, member.getAsJsonObject());
         }
 
-        //write back the team id into the TI
-        Document ti = reqBundle.tool.getParentDatabase().getDocumentByUNID(reqBundle.request.getItemValueString("ToolInstanceUNID"));
+        // write back the team id into the TI
+        Document ti = reqBundle.tool.getParentDatabase()
+                .getDocumentByUNID(reqBundle.request.getItemValueString("ToolInstanceUNID"));
         ti.replaceItemValue("objectId", teamId);
-        ti.save(); 
+        ti.save();
 
-		return new JsonJavaObject("success", "hallu:)");
-	}
-
-    private List<JsonJavaObject> getConversationMembers(List<JsonJavaObject> members) {
-        return members.stream().map(this::getConversationMember).collect(Collectors.toList());
+        return new JsonJavaObject("success", "hallu:)");
     }
 
-    private JsonJavaObject getConversationMember(JsonJavaObject member) {
-        JsonJavaObject conversationMember = new JsonJavaObject();
-        conversationMember.put("@odata.type", "#microsoft.graph.aadUserConversationMember");
-        conversationMember.put("roles", Arrays.asList(member.get("role")));
-        conversationMember.put("user@odata.bind", "https://graph.microsoft.com/v1.0/users('" + member.get("emailaddress") + "')");
+    private JsonArray getConversationMembers(JsonArray members) {
+        JsonArray result = new JsonArray();
+        for (int i = 0; i < members.size(); i++) {
+            JsonObject member = members.get(i).getAsJsonObject();
+            result.add(getConversationMember(member));
+        }
+        return result;
+    }
+
+    private JsonObject getConversationMember(JsonObject member) {
+        JsonObject conversationMember = new JsonObject();
+        conversationMember.addProperty("@odata.type", "#microsoft.graph.aadUserConversationMember");
+        JsonArray roles = new JsonArray();
+        roles.add(member.get("role").getAsString());
+        conversationMember.add("roles", roles);
+        conversationMember.addProperty("user@odata.bind",
+                "https://graph.microsoft.com/v1.0/users('" + member.get("emailaddress").getAsString() + "')");
         return conversationMember;
     }
 
-    private void createUser(String teamId, JsonJavaObject conversationMember) throws Exception {
+    private void createUser(String teamId, JsonObject conversationMember) throws Exception {
         try {
-            api.post("/teams/" + teamId + "/members", conversationMember);
+            api.client.post("/teams/" + teamId + "/members", HttpUtil.createEntity(conversationMember));
         } catch (Exception e) {
             throw new VrhException("Error creating user: " + e.getMessage());
         }
     }
 
-    private String createToolInstance(Object input) throws Exception {
+    @SuppressWarnings("unchecked")
+	private String createToolInstance(Object input) throws Exception {
 
         Map<String, Object> params = (Map<String, Object>) input;
+        TNotesUtil.logEvent(input.toString());
 
         String email = params.get("email").toString();
         String displayName = params.getOrDefault("displayName", "").toString();
         String description = params.getOrDefault("description", "").toString();
-        String template = params.getOrDefault("template", "https://graph.microsoft.com/beta/teamsTemplates('standard')").toString();
-    
+        String template = params.getOrDefault("template", "https://graph.microsoft.com/beta/teamsTemplates('standard')")
+                .toString();
+
         // handle team creation via api
         try {
-            JsonJavaObject newTeam = new JsonJavaObject();
-            newTeam.put("displayName", displayName);
-            newTeam.put("description", description);
-            newTeam.put("template@odata.bind", template);
+            JsonObject newTeam = new JsonObject();
+            newTeam.addProperty("displayName", displayName);
+            newTeam.addProperty("description", description);
+            newTeam.addProperty("template@odata.bind", template);
 
             // the team needs an owner
-            List<JsonJavaObject> members = new ArrayList<>();
-            JsonJavaObject member = new JsonJavaObject();
-            member.put("@odata.type", "#microsoft.graph.aadUserConversationMember");
-            member.put("roles", Arrays.asList("owner"));
-            member.put("user@odata.bind", "https://graph.microsoft.com/v1.0/users('" + email + "')");
+            JsonArray members = new JsonArray();
+            JsonObject member = new JsonObject();
+            member.addProperty("@odata.type", "#microsoft.graph.aadUserConversationMember");
+            
+            JsonArray roles = new JsonArray();
+            roles.add("owner");
+            member.add("roles", roles);
+            
+            member.addProperty("user@odata.bind", "https://graph.microsoft.com/v1.0/users('" + email + "')");
+            
             members.add(member);
-            newTeam.put("members", members);
+            newTeam.add("members", members);
 
-            Object resp = api.post("/teams", newTeam);
+            Response resp = api.client.post("/teams", HttpUtil.createEntity(newTeam));
             return extractTeamIdFromHeaders(resp);
-
-        } catch (Exception e) {   
+        } catch (Exception e) {
             e.printStackTrace();
             throw new VrhException("Error creating tool instance: " + e.getMessage());
         }
-    }	
+    }
 
-    private String extractTeamIdFromHeaders(Object resp) {
-        try {
-            JsonJavaObject response = (JsonJavaObject) resp;
-            Map<String, Object> headers = response.getAsMap("headers");
-            List<String> contentLocationArray = (List<String>) headers.get("Content-Location");
-            String contentLocation = contentLocationArray.get(0);
-    
-            // The team ID is enclosed in single quotes ('), so we can extract it using a regular expression
-            Pattern pattern = Pattern.compile("'(.*?)'");
-            Matcher matcher = pattern.matcher(contentLocation);
-            if (matcher.find()) {
-                return matcher.group(1);
-            } else {
-                throw new Exception("No team ID found in Content-Location header");
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
+    private String extractTeamIdFromHeaders(Response resp) throws Exception {
+        String contentLocation = resp.getHeaders().get("Content-Location");
+
+        // The team ID is enclosed in single quotes ('), so we can extract it using a
+        // regular expression
+        Pattern pattern = Pattern.compile("'(.*?)'");
+        Matcher matcher = pattern.matcher(contentLocation);
+        if (matcher.find()) {
+            return matcher.group(1);
+        } else {
+            throw new Exception("No team ID found in Content-Location header");
         }
+
     }
 }
