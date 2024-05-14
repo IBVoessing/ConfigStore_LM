106109513cd0a3e600b775ac99142e0b275799a5
Leonardo <leonardo.malzacher@voessing.de>
2024-05-14 15:24:38 +0200

    further developed teh TicketAgent should and could work at this state however logic is missing to change exisiting entries based on new Tickets

--- src/Code/Java/com/voessing/calendar/CalendarTicketAgent.java
+++ src/Code/Java/com/voessing/calendar/CalendarTicketAgent.java
@@ -1,20 +1,23 @@
 package com.voessing.calendar;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.openntf.domino.Database;
+import org.openntf.domino.Document;
 import org.openntf.domino.DocumentCollection;
 import org.openntf.domino.Session;
 import org.openntf.domino.utils.Factory;
 import org.openntf.domino.utils.Factory.SessionType;
 
-import lotus.domino.NotesCalendar;
-import lotus.domino.NotesCalendarEntry;
-import lotus.domino.NotesException;
-import lotus.domino.View;
-import lotus.domino.ViewEntry;
-import lotus.domino.ViewEntryCollection;
+import org.openntf.domino.NotesCalendar;
+import org.openntf.domino.NotesCalendarEntry;
+import org.openntf.domino.View;
+import org.openntf.domino.ViewEntry;
+import org.openntf.domino.ViewEntryCollection;
 
 public class CalendarTicketAgent {
     
@@ -63,18 +66,27 @@ public class CalendarTicketAgent {
 
     private List<CalendarTicket> loadOpenTickets() {
         // load all tickets from the database
-        DocumentCollection tickets = azeDb.search("Form = \"CalendarTicket\"");
+        DocumentCollection tickets = azeDb.search("Form = \"CalendarTicket\" & AgentStatus != \"processed\"");
         return tickets.stream().map(CalendarTicket::new).collect(Collectors.toList());
     }
 
     private void processTicket(CalendarTicket ticket) {
-        
+        Document ticketDoc = null;
+        try {
+            MailEntry mailEntry = getUserMailEntry(ticket.getRequester());
+            createCalendarEntry(mailEntry, ticket);
+
+            ticketDoc = azeDb.getDocumentByUNID(ticket.getTicketDocumentUnid());
+            ticketDoc.replaceItemValue("AgentStatus", "processed");
+        } catch (Exception e) {
+            handleTicketError(e, ticketDoc);
+        }
     }
 
-    public void createCalendarEntry(MailEntry mailEntry) throws NotesException {
+    public void createCalendarEntry(MailEntry mailEntry, CalendarTicket ticket) {
         Database userMailDB = serverAgentSession.getDatabase(mailEntry.getMailServer(), mailEntry.getMailFile());
-        
-        if(userMailDB == null) {
+
+        if (userMailDB == null) {
             throw new IllegalArgumentException("Database not found");
         }
 
@@ -82,22 +94,38 @@ public class CalendarTicketAgent {
 
         System.out.println(userMailDB);
         System.out.println(userCalendar);
-        NotesCalendarEntry entry = userCalendar.createEntry(generateICal());
+        NotesCalendarEntry entry = userCalendar.createEntry(generateICal(ticket));
     }
 
-    private String generateICal(){
+    private void handleTicketError(Exception e, Document ticketDoc) {
+        if (ticketDoc != null) {
+            // try to update the ticket document with the error message
+            ticketDoc.replaceItemValue("AgentStatus", "error");
+            ticketDoc.replaceItemValue("AgentError", e.getMessage());
+        } else {
+            // if the ticket document is not available, escalate the error
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String generateICal(CalendarTicket ticket) {
         StringBuilder iCal = new StringBuilder();
         iCal.append("BEGIN:VCALENDAR\n");
         iCal.append("BEGIN:VEVENT\n");
-        iCal.append("DTSTART:20240507T160000\n");
-        iCal.append("DTEND:20240507T180000\n");
-        iCal.append("SUMMARY:Urlaub\n");
+        iCal.append(String.format("DTSTART:%s\n", formatDate(ticket.getStartDate())));
+        iCal.append(String.format("DTEND:%s\n", formatDate(ticket.getEndDate())));
+        iCal.append(String.format("SUMMARY:%s\n", ticket.getTitle()));
         iCal.append("END:VEVENT\n");
         iCal.append("END:VCALENDAR\n");
         return iCal.toString();
     }
 
-    private MailEntry getUserMailEntry(String cnName) throws NotesException {
+    private String formatDate(Date date) {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd'T'HHmmss");
+        return formatter.format(date);
+    }
+
+    private MailEntry getUserMailEntry(String cnName) {
         //TODO: change hardcoded server name to session.getServerName()
         Database namesDB = serverAgentSession.getDatabase("CN=IBVDNO03/O=IBV/C=DE", "names.nsf");
         View userView = namesDB.getView("($Users)");
